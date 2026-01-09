872f4594bef22d9822fdd56d37c11341078c9fdd
Tulip Blossom <tulilirockz@outlook.com>
2026-01-08 21:06:06 -0300

    fix: fedora logo on plymouth
    Signed-off-by: Tulip Blossom <tulilirockz@outlook.com>

--- build_files/01-theme.sh
+++ build_files/01-theme.sh
@@ -139,7 +139,7 @@ systemctl enable brew-setup.service
 systemctl enable flatpak-preinstall.service
 
 git clone "https://github.com/zirconium-dev/zdots.git" /usr/share/zirconium/zdots
-install -Dpm0644 -t /usr/share/plymouth/themes/spinners/ /ctx/assets/logos/watermark.png
+install -Dpm0644 -t /usr/share/plymouth/themes/spinner/ /ctx/assets/logos/watermark.png
 install -Dpm0644 -t /usr/share/zirconium/skel/Pictures/Wallpapers/ /ctx/assets/wallpapers/*
 install -Dpm0644 -t /usr/share/zirconium/pixmaps/ /ctx/assets/logos/logo-z.svg
 
