326f20202b6f7db00c94738afd801ed48f16941b
Valerie <155010216+valerie-tar-gz@users.noreply.github.com>
2026-03-11 11:16:33 +0000

    Fix xdg-terminal-exec-nautilus fetch url

--- build_files/01-theme-fetch.sh
+++ build_files/01-theme-fetch.sh
@@ -101,7 +101,7 @@ dnf install -y \
 git clone "https://github.com/zirconium-dev/zdots.git" /usr/share/zirconium/zdots
 
 XDG_EXT_TMPDIR="$(mktemp -d)"
-curl -fsSLo - "$(curl -fsSL https://api.github.com/repos/tulilirockz/xdg-terminal-exec-nautilus/releases/latest | jq -rc .tarball_url)" | tar -xzvf - -C "${XDG_EXT_TMPDIR}"
+curl -fsSLo - "$(curl -fsSL https://api.github.com/repos/zirconium-dev/xdg-terminal-exec-nautilus/releases/latest | jq -rc .tarball_url)" | tar -xzvf - -C "${XDG_EXT_TMPDIR}"
 install -Dpm0644 -t "/usr/share/nautilus-python/extensions/" "${XDG_EXT_TMPDIR}"/*/xdg-terminal-exec-nautilus.py
 rm -rf "${XDG_EXT_TMPDIR}"
 
